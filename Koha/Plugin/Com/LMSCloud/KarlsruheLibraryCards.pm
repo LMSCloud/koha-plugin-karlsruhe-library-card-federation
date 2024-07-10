bb1cdb6ba0a63eeb6bb8d85093ed1ec0e1e6d631
roger <roger@kallarm.fritz.box>
2024-07-10 11:51:32 +0200

    Initial commit of KarlsruheLibraryCards API functions.

---
+++ Koha/Plugin/Com/LMSCloud/KarlsruheLibraryCards.pm
@@ -0,0 +1,122 @@
+package Koha::Plugin::Com::LMSCloud::KarlsruheLibraryCards;
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program comes with ABSOLUTELY NO WARRANTY;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+
+our $VERSION = "{VERSION}";
+
+our $metadata = {
+    name            => 'Karlsruhe Library Card Federation Plugin',
+    author          => 'LMSCloud GmbH',
+    date_authored   => '2024-07-09',
+    date_updated    => '1900-01-01',
+    minimum_version => '22.11.15.011',
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'This plugin implements API endpoints required'
+                     . ' for the Karlsruhe library federation to verify and set'
+                     . ' status of library cards.'
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+sub api_routes {
+    my ( $self, $args ) = @_;
+
+    my $spec_dir = $self->mbf_dir();
+
+    my $schema = JSON::Validator::Schema::OpenAPIv2->new;
+    my $spec = $schema->resolve($spec_dir . '/openapi.yaml');
+
+    return $self->_convert_refs_to_absolute($spec->data->{'paths'}, 'file://' . $spec_dir . '/');
+}
+
+sub api_namespace {
+    my ( $self ) = @_;
+
+    return 'kalibfed';
+}
+
+# Mandatory even if does nothing
+sub install {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+# Mandatory even if does nothing
+sub upgrade {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+# Mandatory even if does nothing
+sub uninstall {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+sub _convert_refs_to_absolute {
+    my ( $self, $hashref, $path_prefix ) = @_;
+
+    foreach my $key (keys %{ $hashref }) {
+        if ($key eq '$ref') {
+            if ($hashref->{$key} =~ /^(\.\/)?openapi/) {
+                $hashref->{$key} = $path_prefix . $hashref->{$key};
+            }
+        } elsif (ref $hashref->{$key} eq 'HASH' ) {
+            $hashref->{$key} = $self->_convert_refs_to_absolute($hashref->{$key}, $path_prefix);
+        } elsif (ref($hashref->{$key}) eq 'ARRAY') {
+            $hashref->{$key} = $self->_convert_array_refs_to_absolute($hashref->{$key}, $path_prefix);
+        }
+    }
+    return $hashref;
+}
+
+sub _convert_array_refs_to_absolute {
+    my ( $self, $arrayref, $path_prefix ) = @_;
+
+    my @res;
+    foreach my $item (@{ $arrayref }) {
+        if (ref($item) eq 'HASH') {
+            $item = $self->_convert_refs_to_absolute($item, $path_prefix);
+        } elsif (ref($item) eq 'ARRAY') {
+            $item = $self->_convert_array_refs_to_absolute($item, $path_prefix);
+        }
+        push @res, $item;
+    }
+    return \@res;
+}
+
+1;
---
+++ Koha/Plugin/Com/LMSCloud/KarlsruheLibraryCards/LibraryCardController.pm
@@ -0,0 +1,51 @@
+package Koha::Plugin::Com::LMSCloud::KarlsruheLibraryCards::LibraryCardController;
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program comes with ABSOLUTELY NO WARRANTY;
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+sub getCardStatus {
+    my $c = shift->openapi->valid_input or return;
+    my $cardNumber = $c->validation->output->{'card_number'};
+    
+    my $response = { card_number => $cardNumber, card_status => "active" };
+    
+    return $c->render(status  => 200, openapi => $response );
+}
+
+sub setCardStatus {
+    my $c = shift->openapi->valid_input or return;
+    
+    my $body = $c->validation->param('body');
+    my $cardNumber = $body->{card_number};
+    my $cardStatus = $body->{card_status};
+    
+    my $response = { card_number => $cardNumber, card_status => $cardStatus };
+    
+    return $c->render(status  => 200, openapi => $response );
+}
+
+sub healthCheck {
+    my $c = shift->openapi->valid_input or return;
+    
+    my $response = { status => "ok" };
+    
+    return $c->render(status  => 200, openapi => $response );
+}
+
+1;
