f58889e5d8ad1abbb0850a2b060d86d2ec24c6cc
roger <roger@kallarm.fritz.box>
2024-07-16 20:13:39 +0200

    Increase version.

--- Koha/Plugin/Com/LMSCloud/KarlsruheLibraryCards.pm
+++ Koha/Plugin/Com/LMSCloud/KarlsruheLibraryCards.pm
@@ -21,7 +21,7 @@ use base qw(Koha::Plugins::Base);
 
 use C4::Context;
 
-our $VERSION = "0.0.4";
+our $VERSION = "0.0.5";
 
 our $metadata = {
     name            => 'Karlsruhe Library Card Federation Plugin',
